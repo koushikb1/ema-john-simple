543375098ac384763d700e3bfcee593160053122
koushikb1 <koushikbarman466@gmail.com>
2023-03-29 13:36:55 +0600

    add navber

---
+++ src/components/shop/Shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <div>
+      <div className='shop-container'>
+        <div className='product-container'>
+
+            <h2>Product comming here</h2>
+        </div>
+        <div className='cart-container'>
+            <h1>order summary</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Shop;
+
